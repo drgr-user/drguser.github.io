b0e4dfb4a6a5d4c17d66143da306bed9eec49bdf
drguser <drguser@users.noreply.github.com>
2020-02-29 21:27:32 +0330

    webpage v.2; Page is now split in  half. Background is changed.Introduction is added.

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#00001c"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#00001c"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 120,
-        "color": "#ffffff",
+        "color": "#00001c",
         "opacity": 0.4,
         "width": 1
       },
